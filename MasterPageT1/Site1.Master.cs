7b67b69474b4e9cd25a8fa91ded1ec3e4a2eac59
Paulo Ferreira <alunosnet@gmail.com>
2019-03-18 10:35:18 +0000

    Master Pages

---
+++ MasterPageT1/Site1.Master.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MasterPageT1
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //verificar se existe o cookie
+            HttpCookie cookie = Request.Cookies["AvisoT1"];
+            if (cookie != null)
+            {
+                DivLogin.Visible = false;
+                Response.Write(cookie.Value);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DivLogin.Visible = false;
+            //criar um cookie
+            HttpCookie cookie = new HttpCookie("AvisoT1");
+            cookie.Expires = DateTime.Now.AddYears(1);
+            cookie.Value = "Aceitou";
+            //enviar cookie para browser
+            Response.Cookies.Add(cookie);
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            DivLogin.Visible = false;
+            //criar um cookie
+            HttpCookie cookie = new HttpCookie("AvisoT1");
+            cookie.Expires = DateTime.Now.AddYears(1);
+            cookie.Value = "Rejeitou";
+            //enviar cookie para browser
+            Response.Cookies.Add(cookie);
+        }
+    }
+}
---
+++ MasterPageT1/index.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="MasterPageT1.index" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+Isto é a página index
+</asp:Content>
